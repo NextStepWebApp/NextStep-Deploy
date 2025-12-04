4ba1237ec687dfd98567900238012aadba5d341a
Melchizedek Shah <melchizedekshah18@proton.me>
2025-12-03 21:28:33 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-curl -sL https://github.com/NextStepWebApp/NextStep-Deploy/archive/refs/heads/main.zip -o main.zip
-unzip main.zip
+curl -sL https://github.com/NextStepWebApp/NextStep-Deploy/archive/refs/heads/main.tar.gz -o main.tar.gz
+tar -xzf main.tar.gz
 cd NextStep-Deploy-main
 bash archinstaller.sh
